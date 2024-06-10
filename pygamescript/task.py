3416b5479613a61807807545c733ef6b28f576fe
NakanoSanku <alexkaykt@outlook.com>
2024-06-10 22:45:05 +0800

    精简任务接口，基于任务接口实现任务前置代理，后置代理

--- pygamescript/task.py
+++ pygamescript/task.py
@@ -1,13 +1,12 @@
 from abc import ABC, abstractmethod
 from enum import Enum, auto
-from uuid import UUID, uuid4
+from uuid import uuid4
 
 
 class TaskStatus(Enum):
     PENDING = auto()
     RUNNING = auto()
     COMPLETED = auto()
-    CANCELLED = auto()
 
 
 class Task(ABC):
@@ -36,18 +35,8 @@ class Task(ABC):
         pass
 
     @abstractmethod
-    def before_task(self):
-        """前置任务"""
-        pass
-
-    @abstractmethod
     def task(self):
-        """实际任务"""
-        pass
-
-    @abstractmethod
-    def after_task(self):
-        """后置任务"""
+        """任务逻辑实现"""
         pass
 
     @abstractmethod
@@ -59,3 +48,94 @@ class Task(ABC):
     def reset(self):
         """重置任务"""
         pass
+
+
+class TaskProxy(Task):
+    """任务代理抽象类"""
+
+    def __init__(self, task: Task):
+        super().__init__()
+        self._task = task
+        self._status = TaskStatus.PENDING  # 代理任务的状态
+
+    @property
+    def name(self) -> str:
+        """任务名称"""
+        return self._task.name
+
+    @property
+    def proxy_task_status(self) -> TaskStatus:
+        return self._status
+
+    @proxy_task_status.setter
+    def proxy_task_status(self, status: TaskStatus):
+        self._status = status
+
+    @property
+    def status(self) -> TaskStatus:
+        return self._task.status
+
+    def __str__(self) -> str:
+        """任务描述"""
+        return str(self._task)
+
+    def reset(self):
+        """重置任务状态"""
+        self._task.reset()
+        self._status = TaskStatus.PENDING
+
+    @abstractmethod
+    def task(self):
+        pass
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class BeforeTask(TaskProxy):
+    """前置任务代理抽象类"""
+
+    def __init__(self, task: Task):
+        super().__init__(task)
+
+    @abstractmethod
+    def task(self):
+        """
+        前置任务逻辑实现
+        任务完成后必须将self.proxy_task_status设置成TaskStatus.COMPLETED
+        """
+        pass
+
+    def execute(self):
+        """执行任务"""
+        if self._status != TaskStatus.COMPLETED:
+            self.proxy_task_status = TaskStatus.RUNNING
+            self.task()
+        else:
+            self._task.execute()
+
+
+class AfterTask(TaskProxy):
+    """后置任务代理抽象类"""
+
+    def __init__(self, task: Task):
+        super().__init__(task)
+
+    @abstractmethod
+    def task(self):
+        """
+        后置任务逻辑实现
+        任务完成后必须将self.proxy_task_status设置成TaskStatus.COMPLETED
+        """
+        pass
+
+    def execute(self):
+        """执行任务"""
+        if self.status == TaskStatus.COMPLETED:
+            self.proxy_task_status = TaskStatus.RUNNING
+            self.task()
+        else:
+            self._task.execute()
+
+
