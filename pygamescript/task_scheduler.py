ad86e4fb59fa1c39dfe9b1094e38baabb9960096
NakanoSanku <alexkaykt@outlook.com>
2024-06-11 11:27:41 +0800

    精简任务调度器中的私有变量

--- pygamescript/task_scheduler.py
+++ pygamescript/task_scheduler.py
@@ -20,13 +20,11 @@ class TaskSchedulerStatus(Enum):
 class TaskScheduler:
     def __init__(self, sleep_time: int = SLEEP_TIME):
         """
-        :param sleep_time:任务执行间隔时间
+        :param sleep_time:任务执行间隔时间,单位为ms
         """
         self._task_queue: Optional[TaskQueue] = None
         self._work_thread: Optional[threading.Thread] = None
         self._status = TaskSchedulerStatus.PENDING
-        self._running = False
-        self._paused = False
         self._pause_condition = threading.Condition()
         self._sleep_time = sleep_time / 1000
 
@@ -47,9 +45,9 @@ class TaskScheduler:
 
     def _run(self):
         """任务调度器线程运行的内部方法"""
-        while self._running:
+        while self.status == TaskSchedulerStatus.RUNNING:
             with self._pause_condition:
-                while self._paused:
+                while self.status == TaskSchedulerStatus.PAUSED:
                     self._pause_condition.wait()
 
             next_task = self._task_queue.next_task if self._task_queue else None
@@ -66,38 +64,35 @@ class TaskScheduler:
         if self._status != TaskSchedulerStatus.PENDING:
             raise RuntimeError("Scheduler is already running or has been stopped.")
 
-        self._running = True
+        self._status = TaskSchedulerStatus.RUNNING
         self._work_thread = threading.Thread(target=self._run)
         self._work_thread.start()
-        self._status = TaskSchedulerStatus.RUNNING
+
         logger.debug("TaskScheduler started.")
 
     def stop(self):
         """停止任务调度器"""
-        if not self._running:
+        if self.status == TaskSchedulerStatus.PENDING:
             return
-        self._running = False
+        self._status = TaskSchedulerStatus.PENDING
         if self._work_thread:
             self._work_thread.join()
-        self._status = TaskSchedulerStatus.PENDING
         self._task_queue.reset_all_tasks()  # 重置所有任务状态
         logger.debug("TaskScheduler stopped.")
 
     def pause(self):
         """暂停任务调度器"""
-        if not self._running:
+        if self.status != TaskSchedulerStatus.RUNNING:
             raise RuntimeError("Scheduler is not running.")
         with self._pause_condition:
-            self._paused = True
             self._status = TaskSchedulerStatus.PAUSED
         logger.debug("TaskScheduler paused.")
 
     def resume(self):
         """恢复任务调度器"""
-        if not self._running or not self._paused:
+        if self.status != TaskSchedulerStatus.PAUSED:
             raise RuntimeError("Scheduler is not paused.")
         with self._pause_condition:
-            self._paused = False
-            self._pause_condition.notify_all()
             self._status = TaskSchedulerStatus.RUNNING
+            self._pause_condition.notify_all()
         logger.debug("TaskScheduler resumed.")
